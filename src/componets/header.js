40a6f8150d6658727b0612612aaa31aac147d065
Riji12 <riji.vj@semikolen.com>
2024-09-09 12:00:35 +0530

    updated

---
+++ src/componets/header.js
@@ -0,0 +1,14 @@
+// src/components/Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My React App</h1>
+      <b> Home </b>
+      <b>About </b>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/componets/home.js
@@ -0,0 +1,16 @@
+// src/components/Home.js
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <card>hrllo</card>
+      </div>
+
+      <h2>Home Page</h2>
+    </>
+  );
+}
+
+export default Home;
